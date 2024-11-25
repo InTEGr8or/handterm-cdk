cd5ac1c8ed381b8523a95926c7c323463b933e7b
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-11-25 09:27:52 -0800

    Return instead of exit

--- check_env.sh
+++ check_env.sh
@@ -7,7 +7,6 @@ declare -a required_vars=(
     "GITHUB_CLIENT_ID"
     "GITHUB_CLIENT_SECRET"
     "COGNITO_USER_POOL_ID"
-    "COGNITO_APP_CLIENT_ID"
 )
 
 # Check each required variable
@@ -24,8 +23,8 @@ done
 if [ ${#missing_vars[@]} -ne 0 ]; then
     echo "Error: The following required environment variables are missing:"
     printf '%s\n' "${missing_vars[@]}"
-    exit 1
+    return
 fi
 
 echo "All required environment variables are set."
-exit 0
+return
